541478823d5bf2bae32eccc21bee2d55f3e3e11c
unknown <HP@LAPTOP-78712MDO>
2023-05-27 02:37:01 +0700

    last commit

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :customer
   validates_presence_of :customer, :star
+  validates :star, numericality: { less_than_or_equal_to: 5, greater_than: 0,  only_integer: true }
 end
--- app/models/roomtype.rb
+++ app/models/roomtype.rb
@@ -2,4 +2,5 @@ class Roomtype < ApplicationRecord
   belongs_to :room_class
   has_many :bookings
   validates_presence_of :name, :room_class, :room_size, :price_per_night
+  validates_uniqueness_of :name
 end
